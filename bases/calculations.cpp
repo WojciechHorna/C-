dae943c96de2ab40bed07f37ca38c60ebda51624
Wojciech Horna <wojtekhorna@gmail.com>
2025-08-12 00:49:06 +0200

    calcsdonemaybe

--- bases/calculations.cpp
+++ bases/calculations.cpp
@@ -1,1 +1,57 @@
+#include <iostream>
+using namespace std;
 
+// Like I said in previous file 'iostream' lets us making basic math operations
+
+
+int main() {
+
+    cout<<1+2<<endl;
+
+    cout<<3-1<<endl;
+
+    cout<<2*3<<endl;
+
+    cout<<6/2<<endl;
+
+    cout<<endl;    //It's gonna make a new line in terminal
+
+    //Or in a little bit different way
+
+    //Remember 'int' only will work right if the number is whole number
+    int x=6;
+    int y=3;
+
+    cout<<x+y<<endl;
+
+    cout<<x-y<<endl;
+
+    cout<<x*y<<endl;
+
+    cout<<x/y<<endl;
+
+
+    cout<<endl;
+
+
+    //This is a little bit more advanced math operations
+    //We can use 'float' to make it more precise
+
+    float a=5.4;
+    float b=2.1;
+
+    cout<<a+b<<endl;
+
+    cout<<a-b<<endl;
+
+    cout<<a*b<<endl;
+
+    cout<<a/b<<endl;
+
+    //In the beginning you will use those two types of variables the most
+
+
+ cin.ignore();
+ cin.get();
+return 0;
+}
